ffda3084505b1ba553447f13d09b21a87bb6480d
kc-pramista <kc.pramista@gmail.com>
2024-11-12 23:44:12 +0900

    Add files via upload

---
+++ welcome.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>            
+        <script src="https:/cdn.tailwindcss.com"></script>
+        
+    </head>
+    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+<!-- component -->
+<div class="w-full h-full">
+            <dh-component>
+                <div class="flex flex-no-wrap">
+                    <!-- Sidebar starts -->
+                    <!-- Remove class [ hidden ] and replace [ sm:flex ] with [ flex ] -->
+                    <!--- more free and premium Tailwind CSS components at https://tailwinduikit.com/ --->
+                    <div style="min-height: 716px" class="opacity-75 hover:opacity-100 w-64 absolute sm:relative bg-cyan-800 shadow md:h-full flex-col justify-between hidden sm:flex">
+                        <div class="px-8">
+                            <ul class="mt-12">
+                                <li class="flex w-full justify-between text-gray-300 cursor-pointer items-center mb-6">
+                                    <a href="/" class="flex items-center focus:outline-none focus:ring-2 focus:ring-white">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-grid" width="18" height="18" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z"></path>
+                                            <rect x="4" y="4" width="6" height="6" rx="1"></rect>
+                                            <rect x="14" y="4" width="6" height="6" rx="1"></rect>
+                                            <rect x="4" y="14" width="6" height="6" rx="1"></rect>
+                                            <rect x="14" y="14" width="6" height="6" rx="1"></rect>
+                                        </svg>
+                                        <span class="text-sm ml-2">All</span>
+                                    </a>
+                                </li>
+                                <li class="flex w-full justify-between text-gray-400 hover:text-gray-300 cursor-pointer items-center mb-6">
+                                    <a href="javascript:void(0)" class="flex items-center focus:outline-none focus:ring-2 focus:ring-white">
+                                        <svg xmlns="/" class="icon icon-tabler icon-tabler-puzzle" width="18" height="18" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z"></path>
+                                            <path d="M4 7h3a1 1 0 0 0 1 -1v-1a2 2 0 0 1 4 0v1a1 1 0 0 0 1 1h3a1 1 0 0 1 1 1v3a1 1 0 0 0 1 1h1a2 2 0 0 1 0 4h-1a1 1 0 0 0 -1 1v3a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-1a2 2 0 0 0 -4 0v1a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1h1a2 2 0 0 0 0 -4h-1a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1"></path>
+                                        </svg>
+                                        <span class="text-sm ml-2">New biddings</span>
+                                    </a>
+                                    <div class="py-1 px-3 bg-gray-600 rounded text-gray-300 flex items-center justify-center text-xs">8</div>
+                                </li>
+                                <li class="flex w-full justify-between text-gray-400 hover:text-gray-300 cursor-pointer items-center mb-6">
+                                    <a href="javascript:void(0)" class="flex items-center focus:outline-none focus:ring-2 focus:ring-white">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-compass" width="18" height="18" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z"></path>
+                                            <polyline points="8 16 10 10 16 8 14 14 8 16"></polyline>
+                                            <circle cx="12" cy="12" r="9"></circle>
+                                        </svg>
+                                        <span class="text-sm ml-2">Closed bids</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        
+                    </div>
+                </div>
+                
+                
+            </dh-component>
+        </div>
+    </body>
+</html>
